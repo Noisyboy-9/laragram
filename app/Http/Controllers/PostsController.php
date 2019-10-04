ae7c31e03f25af1b8aae45ab630de5a7de698fc4
noisyBoy-9 <sina.shariati@yahoo.com>
2019-10-04 18:10:06 +0330

    added image upload system with vertification of the image to be required and have proper type

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a image in the storage
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'image' => 'required|file|image|mimes:jpeg,png'
+        ]);
+
+       $request->file('image')->storeAs('/images' , $request->file('image')->hashName(), 'public');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ tests/Feature/UploadImageTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+use Tests\TestCase;
+
+class UploadImageTest extends TestCase
+{
+    /** @test **/
+
+    public function a_user_can_upload_image_and_make_new_post()
+    {
+        Storage::fake('public');
+        $image = UploadedFile::fake()->image('test.jpg');
+
+        $this->post('/posts', [
+            'image' => $image
+        ]);
+
+        $this->assertCount(1, Storage::disk('public')->files('images'));
+
+        $this->assertDatabaseHas('posts' , [
+            'path' => 'images/' . $image->hashName()
+        ]);
+    }
+
+    /** @test **/
+    public function an_image_is_required_for_creating_a_new_post()
+    {
+        $this->post('/posts' , [
+            'image' => null
+        ])->assertSessionHasErrors(['image']);
+    }
+
+    /** @test **/
+    public function uploaded_file_must_be_an_image()
+    {
+        $this->post('/posts', [
+            'image' => UploadedFile::fake()->create('test.pdf')
+        ])->assertSessionHasErrors(['image']);
+    }
+}
