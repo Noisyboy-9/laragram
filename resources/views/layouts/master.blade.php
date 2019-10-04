312d7282afd1cf725608f43ec985045ab96ab2fa
noisyBoy-9 <sina.shariati@yahoo.com>
2019-10-04 23:25:05 +0330

    added missed name in image upload form and also added test for showing uploaded image in the screen

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,12 +6,14 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>@yield('title')</title>
 </head>
 <body>
-
     <div id="app">
         @yield('content')
     </div>
+
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,5 +3,15 @@
 @section('title' , 'POSTS')
 
 @section('content')
+    <form class="form" action="/posts" method="POST" enctype="multipart/form-data">
+        @csrf
 
+        <div class="form__group">
+            <input type="file">
+        </div>
+
+        <div class="form__group">
+            <button type="submit" class="btn btn--primary">Upload</button>
+        </div>
+    </form>
 @endsection
